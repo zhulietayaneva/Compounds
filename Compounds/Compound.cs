3c28acfccf3c1b2140bf8ced43b318dabaf5859c
Zhulieta Yaneva <zhulieta.yaneva4@gmail.com>
2021-11-23 10:11:00 +0200

    Add project files.

---
+++ Compounds/Compound.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compounds
+{
+    public class Compound
+    {
+        public string Name { get; private set; }
+        public List<Contents> Contents { get; private set; }
+
+        public Compound(string formula_name)
+        {
+            Name = formula_name;
+        }
+        public int AtomCount()
+        {
+
+        }
+
+        public int ElementCount()
+        {
+            return Name.Count(x => Char.IsUpper(x));
+        }
+
+        public double Mass()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public Element GetElement(int n)
+        {
+
+        }
+
+
+    }
+}
---
+++ Compounds/Element.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compounds
+{
+   public class Element: Compound
+    {
+        public string Notation { get; set; }
+        public double Mass { get; set; }
+
+
+        public Element(string notation)
+        {
+            Notation = notation;
+        }
+
+
+
+    }
+}
